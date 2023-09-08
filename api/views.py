7304e5098cea78d2d05f59054ffbf4d2013df553
Jay <jayeshchauriwar@gmail.com>
2023-09-08 13:51:47 +0530

    first commit

---
+++ api/views.py
@@ -0,0 +1,9 @@
+# api/views.py
+from rest_framework import generics
+from .models import Book
+from .serializers import BookSerializer
+
+class BookListCreateView(generics.ListCreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+        
